ed276fa7f26393bef5756a7defb18fbc9d510a6b
hershyYJ <127376237+hershyYJ@users.noreply.github.com>
2026-08-31 16:08:49 +0900

    feat: add venue name existence check

--- src/main/java/com/foryoung/foryoung/venue/repository/VenueRepository.java
+++ src/main/java/com/foryoung/foryoung/venue/repository/VenueRepository.java
@@ -1,8 +1,6 @@
 package com.foryoung.foryoung.venue.repository;
 
 import com.foryoung.foryoung.venue.entity.Venue;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
@@ -10,7 +8,7 @@ import java.util.Optional;
 public interface VenueRepository extends JpaRepository<Venue, Long> {
 
 
-    Page<Venue> findByNameContainingIgnoreCase(String keyword, Pageable pageable);
+    boolean existsByName(String name);
 
 
     Optional<Venue> findByName(String name);
