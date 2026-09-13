dedca99a2a308ad62eec822f9902ea2c87b9c9a7
hershyYJ <127376237+hershyYJ@users.noreply.github.com>
2026-09-13 16:01:38 +0900

    refactor: update performance statistics API path

--- src/main/java/com/foryoung/foryoung/statistics/controller/PerformanceStatisticsController.java
+++ src/main/java/com/foryoung/foryoung/statistics/controller/PerformanceStatisticsController.java
@@ -16,7 +16,7 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/performances/reports")
+@RequestMapping("/performance-statistics")
 public class PerformanceStatisticsController {
 
 
